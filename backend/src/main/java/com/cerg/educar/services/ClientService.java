d92dc7ca79710315e979a2cc994a96400886d54e
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-14 10:39:32 -0300

    FindAll Pagination

--- backend/src/main/java/com/cerg/educar/services/ClientService.java
+++ backend/src/main/java/com/cerg/educar/services/ClientService.java
@@ -1,8 +1,22 @@
 package com.cerg.educar.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import com.cerg.educar.dto.ClientDTO;
+import com.cerg.educar.entities.Client;
+import com.cerg.educar.repositories.ClientRepository;
 
 @Service
 public class ClientService {
 
+	@Autowired
+	ClientRepository repository;
+	
+	public Page<ClientDTO> findAll(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
 }
