cc2f3cd0b0300569e0e4deadff18720e99f17756
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-13 23:21:23 -0300

    DataBase Seeding

--- backend/src/main/java/com/cerg/educar/controllers/ClientController.java
+++ backend/src/main/java/com/cerg/educar/controllers/ClientController.java
@@ -1,5 +1,10 @@
 package com.cerg.educar.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/client")
 public class ClientController {
 
 }
