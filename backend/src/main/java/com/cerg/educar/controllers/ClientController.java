fcb6e407fcd6b470b092512e7efced30b355cfe9
Felipe Kinupes Gouvêa <felipekinupesg@hotmail.com>
2021-05-15 12:10:59 -0300

    Insert

--- backend/src/main/java/com/cerg/educar/controllers/ClientController.java
+++ backend/src/main/java/com/cerg/educar/controllers/ClientController.java
@@ -1,5 +1,7 @@
 package com.cerg.educar.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,9 +9,12 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cerg.educar.dto.ClientDTO;
 import com.cerg.educar.services.ClientService;
@@ -39,7 +44,14 @@ public class ClientController {
 		ClientDTO dto = clientService.findById(id);
 		return ResponseEntity.ok().body(dto);		
 	}
-
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+		dto = clientService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				  .buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 }
 
 
--- backend/src/main/java/com/cerg/educar/services/ClientService.java
+++ backend/src/main/java/com/cerg/educar/services/ClientService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cerg.educar.dto.ClientDTO;
 import com.cerg.educar.entities.Client;
@@ -18,14 +19,28 @@ public class ClientService {
 	@Autowired
 	ClientRepository repository;
 	
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
 	}
 
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ControllerNotFoundException("Cliente com id " + id + " não exite!"));
 		return new ClientDTO(entity);	
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);		
+		return new ClientDTO(entity);
 	}
 }
